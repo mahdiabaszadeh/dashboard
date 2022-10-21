846bac83511c58a6c47ee59def2a315f1f115989
mehdi.a <mahdiabaszadeh1995@gmail.com>
2022-10-21 16:22:43 +0330

    set structure

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+// project import
+import Routes from 'routes';
+import ThemeCustomization from 'themes';
+import Locales from 'components/Locales';
+import RTLLayout from 'components/RTLLayout';
+import ScrollTop from 'components/ScrollTop';
+import Snackbar from 'components/@extended/Snackbar';
+
+// auth provider
+// import { AWSCognitoProvider as AuthProvider } from 'contexts/AWSCognitoContext';
+import { JWTProvider as AuthProvider } from 'contexts/JWTContext';
+// import { Auth0Provider as AuthProvider } from 'contexts/Auth0Context';
+
+// ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
+
+const App = () => (
+  <ThemeCustomization>
+    <RTLLayout>
+      <Locales>
+        <ScrollTop>
+          <AuthProvider>
+            <>
+              <Routes />
+              <Snackbar />
+            </>
+          </AuthProvider>
+        </ScrollTop>
+      </Locales>
+    </RTLLayout>
+  </ThemeCustomization>
+);
+
+export default App;
